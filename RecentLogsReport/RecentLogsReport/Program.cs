3baf9864bf8cf1a84f9bdf90f48167cef84876a5
AgileEagle <54956661+AgileEagle@users.noreply.github.com>
2023-02-21 15:48:38 -0600

    Shell of RecentLogsReport

---
+++ RecentLogsReport/RecentLogsReport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecentLogsReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
